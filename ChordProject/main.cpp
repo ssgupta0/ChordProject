e1231b5c97efcce177df9c8da1388dedfb7cf24d
ssgupta2 <shashvatguptausa@gmail.com>
2020-06-07 12:24:44 -0700

    Initial Commit

---
+++ ChordProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChordProject
+//
+//  Created by Shashvat Gupta on 6/7/20.
+//  Copyright © 2020 Shashvat Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
